fd327657f6c8ce4abf6faea5dae86e63e7afc659
Masafumi <masafumi.endo@keio.jp>
2024-03-06 20:20:48 +0900

    change method arguments

--- src/prediction_models/trainers/utils.py
+++ src/prediction_models/trainers/utils.py
@@ -19,6 +19,7 @@ class ParamsModelTraining:
     - learning_rate (float): Learning rate for optimization.
     - weight_decay (float): Weight decay for optimization.
     - num_epochs (int): Number of epochs for training.
+    - num_iterations (int): Number of iterations for training.
     - save_interval (int): Interval for saving model checkpoints.
     - device (str): Device for training, either "cpu" or "cuda".
     """
@@ -27,6 +28,7 @@ class ParamsModelTraining:
     learning_rate: Optional[float] = field(default=None)
     weight_decay: Optional[float] = field(default=None)
     num_epochs: Optional[int] = field(default=None)
+    num_iterations: Optional[int] = field(default=None)
     save_interval: Optional[int] = field(default=None)
     device: Optional[str] = field(default=None)
 
@@ -36,31 +38,21 @@ class ParamsModelTraining:
             raise ValueError("device must be 'cpu', 'cuda', or 'cuda:<index>'.")
 
 
-def load_model_state_dict(
-    model: Module, model_directory: str, params_model_training: ParamsModelTraining
-) -> Module:
+def load_model_state_dict(model: Module, model_directory: str, device: str) -> Module:
     """
     Load a trained model from a file.
 
     Parameters:
     - model (Module): The model need to be loaded.
-    - model_directory (str): Directory containing the model file. No need to specify training parameters.
-    - params_model_training (ParamsModelTraining): Parameters for model training.
+    - model_directory (str): Directory containing the trained model.
+    - device (str): Device for loading the model, either "cpu" or "cuda".
 
     Returns:
     - Module: The trained model.
     """
-    model_directory = os.path.join(
-        model_directory,
-        f"bs{params_model_training.batch_size:03d}_"
-        f"lr{params_model_training.learning_rate:.0e}_"
-        f"wd{params_model_training.weight_decay:.0e}_"
-        f"epochs{params_model_training.num_epochs:03d}",
-        "models/best_model.pth",
-    )
+    if device not in ["cpu", "cuda"] and not device.startswith("cuda:"):
+        raise ValueError("device must be 'cpu', 'cuda', or 'cuda:<index>'.")
 
-    model.load_state_dict(
-        torch.load(model_directory, map_location=params_model_training.device)
-    )
+    model.load_state_dict(torch.load(model_directory, map_location=device))
     model.eval()
     return model
