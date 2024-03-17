241e0ecb7d5e61198661d6122672e36d3cad6da2
Masafumi <masafumi.endo@keio.jp>
2024-03-17 17:19:31 +0900

    modify data handling including batch dimension

--- src/environments/grid_map.py
+++ src/environments/grid_map.py
@@ -4,7 +4,7 @@ Masafumi Endo, 2024
 
 import torch
 from torch.distributions import Normal
-from typing import Optional, Tuple, Dict
+from typing import Optional, Tuple, Union
 
 from src.utils.utils import set_randomness
 
@@ -30,6 +30,13 @@ class GridMap:
         - distributions (Optional[dict[str, Normal]]): Distributions for terrain information.
         - device (Optional[str]): Device to run the model on.
         """
+        self.device = (
+            device
+            if device is not None
+            else "cuda"
+            if torch.cuda.is_available()
+            else "cpu"
+        )
         self.grid_size = grid_size
         self.resolution = resolution
         self.center_x = self.center_y = grid_size * resolution / 2
@@ -47,13 +54,9 @@ class GridMap:
         self.tensors, self.distributions = self.initialize_terrain_data(
             grid_size, tensors, distributions
         )
-        self.device = (
-            device
-            if device is not None
-            else "cuda"
-            if torch.cuda.is_available()
-            else "cpu"
-        )
+        # Move the tensors and distributions to the device
+        self.move_to_device(self.tensors)
+        self.move_to_device(self.distributions)
 
     @staticmethod
     def initialize_terrain_data(
@@ -109,14 +112,35 @@ class GridMap:
         )
         return tensors, distributions
 
-    def get_values_from_positions(self, positions: torch.Tensor, attribute: str):
+    def move_to_device(
+        self, tensor_dict: dict[str, Union[torch.Tensor, Normal]]
+    ) -> None:
+        """
+        Move the tensors and distributions to the device.
+
+        Parameters:
+        - tensor_dict (dict[str, Union[torch.Tensor, Normal]]): Dictionary of tensors and distributions to move to the device.
+        """
+        for key, value in tensor_dict.items():
+            if isinstance(value, torch.Tensor):
+                tensor_dict[key] = value.to(self.device)
+            elif hasattr(value, "mean") and hasattr(value, "stddev"):
+                tensor_dict[key] = Normal(
+                    value.mean.to(self.device), value.stddev.to(self.device)
+                )
+            else:
+                raise TypeError("tensor_dict must contain tensors or distributions.")
+
+    def get_values_at_positions(
+        self, tensor_data: Union[torch.Tensor, Normal], positions: torch.Tensor
+    ):
         """
         Get corresponding value information or a masked distribution for batches of position data across multiple trajectories,
         based on a specified attribute.
 
         Parameters:
+        - tensor_data (Union[torch.Tensor, Normal]): A tensor or a distribution to fetch values from.
         - positions (torch.Tensor): A tensor of shape [batch_size, num_positions, 3] where each row corresponds to (x, y, theta) for each state.
-        - attribute (str): A string specifying which attribute to retrieve (e.g., 'heights', 'slopes', 'latent_models').
 
         Returns:
         - A tensor containing the requested value information or a masked distribution for the states across all trajectories, based on the specified attribute.
@@ -127,26 +151,23 @@ class GridMap:
         # Flatten indices to use advanced indexing, handle 3D -> 2D conversion
         flat_indices = indices.view(-1, 2)
 
-        if attribute in self.tensors:
+        if isinstance(tensor_data, torch.Tensor):
             # Fetching from tensors
-            fetched_values = self.tensors[attribute][
-                flat_indices[:, 0], flat_indices[:, 1]
-            ].view(batch_size, num_positions)
-            return fetched_values
-        elif attribute in self.distributions:
-            # Handling distributions: create a masked distribution based on indices.
-            distribution = self.distributions[attribute]
-            means = distribution.mean[flat_indices[:, 0], flat_indices[:, 1]].view(
+            fetched_values = tensor_data[flat_indices[:, 0], flat_indices[:, 1]].view(
                 batch_size, num_positions
             )
-            stddevs = distribution.stddev[flat_indices[:, 0], flat_indices[:, 1]].view(
+            return fetched_values
+        elif hasattr(tensor_data, "mean") and hasattr(tensor_data, "stddev"):
+            # Handling distributions: create a masked distribution based on indices.
+            means = tensor_data.mean[flat_indices[:, 0], flat_indices[:, 1]].view(
+                batch_size, num_positions
+            )
+            stddevs = tensor_data.stddev[flat_indices[:, 0], flat_indices[:, 1]].view(
                 batch_size, num_positions
             )
             return Normal(means, stddevs)
         else:
-            raise KeyError(
-                f"Attribute '{attribute}' not found in tensors or distributions."
-            )
+            raise TypeError("tensor_data must be a tensor or a distribution.")
 
     def get_grid_indices_from_positions(self, positions: torch.Tensor) -> torch.Tensor:
         """
