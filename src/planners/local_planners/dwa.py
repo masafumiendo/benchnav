45f53ab78d3e791e712aec6b4b65ec27119b99b3
Masafumi <masafumi.endo@keio.jp>
2024-03-20 19:27:47 +0900

    DWA implementation (WIP: global planner ref integration)

---
+++ src/planners/local_planners/dwa.py
@@ -0,0 +1,246 @@
+"""
+Masafumi Endo, 2024.
+"""
+
+from __future__ import annotations
+
+from typing import Callable, Tuple
+
+import torch
+import torch.nn as nn
+
+
+class DWA(nn.Module):
+    """
+    Dynamic Window Approach, 
+    J. Fox et al., IROS, 1997.
+    """
+
+    def __init__(
+        self,
+        horizon: int,
+        dim_state: int,
+        dim_control: int,
+        dynamics: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        stage_cost: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        terminal_cost: Callable[[torch.Tensor], torch.Tensor],
+        u_min: torch.Tensor,
+        u_max: torch.Tensor,
+        a_lim: torch.Tensor,
+        delta_t: float,
+        num_lin_vel: int = 10,
+        num_ang_vel: int = 10,
+        device=torch.device("cuda"),
+        dtype=torch.float32,
+        seed: int = 42,
+    ) -> None:
+        """
+        Initialize the Dynamic Window Approach.
+
+        Parameters:
+        - horizon (int): Predictive horizon length.
+        - dim_state (int): Dimension of state.
+        - dim_control (int): Dimension of control.
+        - dynamics (Callable[[torch.Tensor, torch.Tensor], torch.Tensor]): Dynamics model.
+        - stage_cost (Callable[[torch.Tensor, torch.Tensor], torch.Tensor]): Stage cost.
+        - terminal_cost (Callable[[torch.Tensor], torch.Tensor]): Terminal cost.
+        - u_min (torch.Tensor): Minimum control input.
+        - u_max (torch.Tensor): Maximum control input.
+        - a_lim (torch.Tensor): Maximum acceleration.
+        - delta_t (float): Time step for simulation [s].
+        - num_lin_vel (int): Number of linear velocity samples.
+        - num_ang_vel (int): Number of angular velocity samples.
+        - device (torch.device): Device to run the solver.
+        - dtype (torch.dtype): Data type to run the solver.
+        - seed (int): Seed for torch.
+        """
+        super().__init__()
+
+        # torch seed
+        torch.manual_seed(seed)
+
+        # check dimensions
+        assert u_min.shape == (
+            dim_control,
+        ), "u_min must be a tensor of shape (dim_control,)"
+        assert u_max.shape == (
+            dim_control,
+        ), "u_max must be a tensor of shape (dim_control,)"
+        assert a_lim.shape == (
+            dim_control,
+        ), "a_lim must be a tensor of shape (dim_control,)"
+
+        # device and dtype
+        if torch.cuda.is_available() and device == torch.device("cuda"):
+            self._device = torch.device("cuda")
+        else:
+            self._device = torch.device("cpu")
+        self._dtype = dtype
+
+        self._horizon = horizon
+        self._dim_state = dim_state
+        self._dim_control = dim_control
+        self._dynamics = dynamics
+        self._stage_cost = stage_cost
+        self._terminal_cost = terminal_cost
+        self._u_min = u_min.clone().detach().to(self._device, self._dtype)
+        self._u_max = u_max.clone().detach().to(self._device, self._dtype)
+        self._a_lim = a_lim.clone().detach().to(self._device, self._dtype)
+        self._delta_t = delta_t
+        self._num_lin_vel = num_lin_vel
+        self._num_ang_vel = num_ang_vel
+
+        self._previous_action_seq = torch.zeros(
+            self._horizon, self._dim_control, device=self._device, dtype=self._dtype
+        )
+
+        self._state_seq_batch = torch.zeros(
+            self._num_lin_vel * self._num_ang_vel,
+            self._horizon + 1,
+            self._dim_state,
+            device=self._device,
+            dtype=self._dtype,
+        )
+        self._weights = torch.zeros(
+            self._num_lin_vel * self._num_ang_vel,
+            device=self._device,
+            dtype=self._dtype,
+        )
+
+    def forward(self, state: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Compute the optimal control input.
+
+        Parameters:
+        - state (torch.Tensor): Current state of the robot as a tensor (x, y, theta).
+
+        Returns:
+        - optimal_action_seq (torch.Tensor): Optimal action sequence.
+        - optimal_state_seq (torch.Tensor): Optimal state sequence.
+        """
+        assert state.shape == (
+            self._dim_state,
+        ), "state must be a tensor of shape (dim_state,)"
+
+        # Check input tensor is on the same device and data type
+        if not torch.is_tensor(state):
+            state = torch.tensor(state, device=self._device, dtype=self._dtype)
+        if state.device != self._device:
+            state = state.to(self._device, self._dtype)
+
+        actions = self._generate_actions()
+
+        # Simulate state sequences and compute costs
+        state_seq_batch = self._simulate_state_sequences(state, actions)
+        cost_batch = self._compute_costs(state_seq_batch, actions)
+
+        # Find the optimal action and state sequences based on the minimum cost
+        min_cost_index = torch.argmin(cost_batch, dim=0)
+        optimal_action_seq = actions[min_cost_index].unsqueeze(0)
+        optimal_state_seq = state_seq_batch[min_cost_index].unsqueeze(0)
+
+        # Update previous action sequence
+        self._previous_action_seq = optimal_action_seq
+        self._state_seq_batch = state_seq_batch
+        self._weights = torch.softmax(-cost_batch, dim=0)
+
+        return optimal_action_seq, optimal_state_seq
+
+    def _generate_actions(self) -> torch.Tensor:
+        """
+        Generate set of actions within the dynamic window based on the current action and maximum acceleration.
+
+        Parameters:
+
+        Returns:
+        - actions (torch.Tensor): Set of actions within the dynamic window.
+        """
+        # Adjust action limits based on the current action and maximum acceleration
+        action = self._previous_action_seq[0, :]
+
+        v_min = torch.max(self._u_min[0], action[0] - self._a_lim[0] * self._delta_t)
+        v_max = torch.min(self._u_max[0], action[0] + self._a_lim[0] * self._delta_t)
+        omega_min = torch.max(
+            self._u_min[1], action[1] - self._a_lim[1] * self._delta_t
+        )
+        omega_max = torch.min(
+            self._u_max[1], action[1] + self._a_lim[1] * self._delta_t
+        )
+
+        # Generate linear and angular velocities within the dynamic window
+        vs = torch.linspace(
+            v_min, v_max, self._num_lin_vel, device=self._device, dtype=self._dtype
+        )
+        omegas = torch.linspace(
+            omega_min,
+            omega_max,
+            self._num_ang_vel,
+            device=self._device,
+            dtype=self._dtype,
+        )
+        actions = torch.cartesian_prod(vs, omegas)
+        return actions
+
+    def _simulate_state_sequences(
+        self, state: torch.Tensor, actions: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Simulate the state sequence based on the current state and action.
+
+        Parameters:
+        - state (torch.Tensor): Current state of the robot as a tensor (x, y, theta).
+        - actions (torch.Tensor): All possible actions as a batch of control tensors shaped [num_actions, 2].
+
+        Returns:
+        - state_seq_batch (torch.Tensor): State sequences.
+        """
+        state_seq_batch = torch.zeros(
+            actions.shape[0],
+            self._horizon + 1,
+            3,
+            device=self._device,
+            dtype=self._dtype,
+        )
+        state_seq_batch[:, 0, :] = state.unsqueeze(0).repeat(actions.shape[0], 1)
+        for t in range(self._horizon):
+            state_seq_batch[:, t + 1, :] = self._dynamics(
+                state_seq_batch[:, t, :], actions
+            )
+        return state_seq_batch
+
+    def _compute_costs(
+        self, state_seq_batch: torch.Tensor, actions: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Compute the cost based on the state sequence.
+
+        Parameters:
+        - state_seq_batch (torch.Tensor): Batch of state sequences.
+        - actions (torch.Tensor): All possible actions as a batch of control tensors shaped [num_actions, horizon, 2].
+
+        Returns:
+        - cost_batch (torch.Tensor): Total costs.
+        """
+        cost_batch = torch.zeros(
+            state_seq_batch.shape[0], device=self._device, dtype=self._dtype
+        )
+        for t in range(self._horizon):
+            cost_batch += self._stage_cost(state_seq_batch[:, t, :], actions)
+
+        cost_batch += self._terminal_cost(state_seq_batch[:, -1, :])
+
+        return cost_batch
+
+    def get_top_samples(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Get top samples 
+
+        Returns:
+        - top_samples (torch.Tensor): Top samples.
+        - top_weights (torch.Tensor): Top weights.
+        """
+        top_indices = torch.argsort(self._weights, descending=True)
+        top_samples = self._state_seq_batch[top_indices]
+        top_weights = self._weights[top_indices]
+
+        return top_samples, top_weights
--- src/planners/local_planners/objectives.py
+++ src/planners/local_planners/objectives.py
@@ -3,6 +3,7 @@ Masafumi Endo, 2024.
 """
 
 from __future__ import annotations
+from typing import Optional
 
 import torch
 
@@ -10,16 +11,23 @@ from src.simulator.robot_model import UnicycleModel
 
 
 class Objectives:
-    def __init__(self, dynamics: UnicycleModel, goal_pos: torch.Tensor) -> None:
+    def __init__(
+        self,
+        dynamics: UnicycleModel,
+        goal_pos: torch.Tensor,
+        global_path: Optional[torch.Tensor] = None,
+    ) -> None:
         """
         Initialize the objectives for the motion planning problem.
 
         Parameters:
         - dynamics (UnicycleModel): Dynamics model of the robot.
         - goal_pos (torch.Tensor): Goal position of the robot for the stage cost.
+        - global_path (Optional[torch.Tensor]): Global path for the path following cost.
         """
         self._dynamics = dynamics
         self._goal_pos = goal_pos
+        self._global_path = global_path
 
     def stage_cost(
         self, state: torch.Tensor, action: torch.Tensor, stuck_threshold: float = 0.1
@@ -41,7 +49,10 @@ class Objectives:
         trav = self._dynamics.get_traversability(state.unsqueeze(1)).squeeze(1)
         collision_cost = trav <= stuck_threshold
 
-        return goal_cost + 1e4 * collision_cost
+        # Compute path following cost
+        path_following_cost = self.path_following_cost(state)
+
+        return goal_cost + 1e4 * collision_cost + path_following_cost
 
     def terminal_cost(self, state: torch.Tensor) -> torch.Tensor:
         """
@@ -54,3 +65,21 @@ class Objectives:
         - cost (torch.Tensor): Terminal cost at the given position as batch of cost tensors shaped [batch_size].
         """
         return self.stage_cost(state, torch.zeros_like(state[:, :2]))
+
+    def path_following_cost(self, state: torch.Tensor) -> torch.Tensor:
+        """
+        Compute path following cost at the given position.
+
+        Parameters:
+        - state (torch.Tensor): State of the robot as batch of position tensors shaped [batch_size, 3].
+
+        Returns:
+        - cost (torch.Tensor): Path following cost at the given position as batch of cost tensors shaped [batch_size].
+        """
+        # Return zero cost if no global path is given
+        if self._global_path is None or self._global_path.shape[0] == 0:
+            return torch.zeros(state.shape[0], device=state.device)
+
+        # Compute the distance to the global path
+        dists = torch.cdist(state[:, :2], self._global_path[:, :2])
+        return dists.min(dim=1).values
